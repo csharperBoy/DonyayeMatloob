d82c6eaac87d9132a03cebe46927d53b71343dc6
868 <868@aksteel>
2025-03-27 13:49:55 +0330

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,19 +3,17 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 const config: Config = withMT({
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     fontFamily: {
-      vazir: ["Vazirmatn", "sans-serif"], // فونت اصلی منوها
-      yekan: ["Yekan", "sans-serif"], // فونت عنوان سایت
-      iransans: ["IRANSans", "sans-serif"], // فونت دکمه‌ها
+      vazir: ["var(--font-vazirmatn)", "sans-serif"],
     },
     extend: {
       colors: {
         purple: {
-          600: '#7e22ce', // رنگ بنفش اصلی
+          600: '#7e22ce',
           700: '#6b21a8',
         }
       }
